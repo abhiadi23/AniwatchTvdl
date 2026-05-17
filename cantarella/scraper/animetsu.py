8b6723738fddd417194f7a27a7bb34c283f115fd
Seishiro <abhinavaditya035@gmail.com>
2026-05-17 11:29:42 +0530

    Update animetsu.py

--- cantarella/scraper/animetsu.py
+++ cantarella/scraper/animetsu.py
@@ -11,7 +11,7 @@ from pathlib import Path
 class AnimetsuScraper:
     BASE_URL = "https://animetsu.live"
     API_URL = f"{BASE_URL}/v2/api"
-    PROXY_URL = "https://mega-cloud.top/proxy" # Fallback if need_proxy is true
+    PROXY_URL = "https://swiftstream.top/proxy" # Fallback if need_proxy is true
 
     HEADERS = {
         "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36",
