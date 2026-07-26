fbf2c31829ec8b8b660d76d4e2c24ddeae8ec061
Seishiro <abhinavaditya035@gmail.com>
2026-07-26 12:15:33 +0530

    Combine permission setting and application launch

--- run.sh
+++ run.sh
@@ -1,16 +1,1 @@
-#!/bin/bash
-
-# 1. Grant execution permissions to your native downloader binary
-echo "Setting executable permissions on N_m3u8DL-RE..."
-chmod +x binary/N_m3u8DL-RE
-
-# 2. Start the native Tor daemon in the background
-echo "Spinning up Tor infrastructure proxy..."
-tor --RunAsDaemon 1 --SocksPort 9050
-
-# 3. Give Tor a few seconds to successfully bind to port 9050
-sleep 4
-
-# 4. Fire up your Python package wrapper
-echo "Launching main cantarella application framework..."
-python3 -m cantarella
+chmod +x binary/N_m3u8DL-RE && python3 -m cantarella
